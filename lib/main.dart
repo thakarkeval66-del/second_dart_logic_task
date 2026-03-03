cd2bfc08e0d1014860a57337ca146915ac4f835f
keval <keval.thakar@discus.solutions>
2026-03-04 03:10:55 +0530

    -> final sorted list is done with the old school method of bubble sort logic tested code give exact out put which we wanted.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,38 @@ void main(){
   // Step 1: Remove duplicates
   final List<int> uniqueElements = _removeDuplicates(inputList);
   print('List after removing duplicates: $uniqueElements');
+
+  // Step 2: Sort the remaining unique elements
+  final List<int> finalSortedList = _bubbleSort(uniqueElements);
+  _bubbleSort(uniqueElements);
+
+  // Step 3: Print the final sorted list
+  print('Final sorted list (unique elements): $finalSortedList');
+}
+
+List<int> _bubbleSort(List<int> list) {
+  if (list.isEmpty) {
+    return [];
+  }
+
+  final List<int> sortedList = [];
+  for (final int element in list) {
+    sortedList.add(element);
+  }
+  final int n = sortedList.length;
+
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      // Compare adjacent elements
+      if (sortedList[j] > sortedList[j + 1]) {
+        // Swap elements if they are in the wrong order
+        final int temp = sortedList[j];
+        sortedList[j] = sortedList[j + 1];
+        sortedList[j + 1] = temp;
+      }
+    }
+  }
+  return sortedList;
 }
 
 List<int> _removeDuplicates(List<int> originalList) {
